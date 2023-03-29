bd73549a103173d1f7690f846d8717a2fa564964
Gavin Challice <nivag_17@hotmail.com>
2023-03-29 22:51:02 +0100

    Add check to see if movie is already in Db.

--- Pages/Movies/Create.cshtml.cs
+++ Pages/Movies/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using VampireMedia.Data;
 using VampireMedia.Models;
 
@@ -13,6 +14,7 @@ namespace VampireMedia.Pages.Movies
     public class CreateModel : PageModel
     {
         private readonly VampireMedia.Data.VampireMediaContext _context;
+        public string MovieExistsMessage { get; set; }
 
         public CreateModel(VampireMedia.Data.VampireMediaContext context)
         {
@@ -36,10 +38,34 @@ namespace VampireMedia.Pages.Movies
                 return Page();
             }
 
+            // Is the movie already in the Db?
+            if (await GetMoviesToCheck(Movies))
+            {
+                MovieExistsMessage = "This movie already exists in the database on this media type.";
+                return RedirectToPage("./Index");
+            }
+               
+               
             _context.Movies.Add(Movies);
             await _context.SaveChangesAsync();
-
             return RedirectToPage("./Index");
+        }
+
+
+        private async Task<bool> GetMoviesToCheck(MoviesModel movies)
+        {
+            var currentMoviesInDb = await _context.Movies.OrderBy(x => x.Title).ToListAsync();
+            
+            foreach(var movie in currentMoviesInDb)
+            {
+                if(movie.Title == movies.Title && movie.Director == movies.Director && 
+                    movie.ReleaseDate == movies.ReleaseDate && 
+                    movie.MediaType == movies.MediaType)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
